951c62537a9fa53c2c1ad791e58ccbb72ecf627e
elpsycong003 <garciadss100@gmail.com>
2023-04-02 04:56:23 -0400

    just edited the text

--- index.js
+++ index.js
@@ -3,35 +3,36 @@
 // 8-Rnubess4qCcjjdy
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
 
-    emailjs
+  emailjs
     .sendForm(
-        'service_rooke27',
-        'template_8wem99i',
-        event.target,
-        '8-Rnubess4qCcjjdy'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }).catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-            "The email service is temporarily unavailable. Please contact me directly at garciadss100@gmail.com"
-        );
+      "service_rooke27",
+      "template_8wem99i",
+      event.target,
+      "8-Rnubess4qCcjjdy"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
     })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at garciadss100@gmail.com"
+      );
+    });
 }
 
 let isModalOpen = false;
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
-
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
