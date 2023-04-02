d83bc01b3318d87527af4d86b5dbb4a717eedf97
elpsycong003 <garciadss100@gmail.com>
2023-04-02 05:28:07 -0400

    variables to the top of the js page+added toggle

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // template_8wem99i
 // service_rooke27
 // 8-Rnubess4qCcjjdy
+let isModalOpen = false;
+toggleContrast
 
 function contact(event) {
   event.preventDefault();
@@ -27,7 +29,6 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
