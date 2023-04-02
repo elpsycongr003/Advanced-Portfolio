74f373dede3bd810c52b1497657bcba3515b1a95
elpsycong003 <garciadss100@gmail.com>
2023-04-02 02:45:35 -0400

    reconnected email.js+tested an error if it worked

--- index.js
+++ index.js
@@ -4,19 +4,29 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    // .sendForm(
-    //     'service_rooke27',
-    //     'template_8wem99i',
-    //     event.target,
-    //     '8-Rnubess4qCcjjdy'
-    // ).then(() => {
-    //     console.log('this worked')
-    // })
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    
+
+    emailjs
+    .sendForm(
+        'service_rooke27',
+        'template_8wem99i',
+        event.target,
+        '8-Rnubess4qCcjjdy'
+    ).then(() => {
+        throw new Error("error");
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at garciadss100@gmail.com"
+        );
+    })
+
+    loading.classList += " modal__overlay--visible" 
     setTimeout(() => {
+
         console.log('it worked2')
-    }, 500);
+    }, 1000);
 }
