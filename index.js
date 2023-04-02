21cc8ade2c969c1d4414768a4aa1ad0f76821e0d
elpsycong003 <garciadss100@gmail.com>
2023-04-02 02:22:43 -0400

    synced in the email.js+ conole tested it

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 // service_rooke27
 // 8-Rnubess4qCcjjdy
 
-function contact {
-    console.log{'it worked'}
+function contact(event) {
+    event.preventDefault();
+    emailjs
+    .sendForm(
+        'service_rooke27',
+        'template_8wem99i',
+        event.target,
+        '8-Rnubess4qCcjjdy'
+    ).then(() => {
+        console.log('this worked')
+    })
+    
 }
